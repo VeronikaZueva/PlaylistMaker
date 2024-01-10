c7b68d5b2b6a52c67e2068acda50cbba3c02589b
VeronikaZueva <zuevavero@yandex.ru>
2024-01-10 22:56:37 +0500

    Home Work 2. Playlist Maker

--- app/src/main/java/com/iclean/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/iclean/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,5 @@
 package com.iclean.playlistmaker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
@@ -13,8 +12,7 @@ class SettingsActivity : AppCompatActivity() {
         //Возвращаемся домой
         val backButton = findViewById<ImageButton>(R.id.back_button)
         backButton.setOnClickListener {
-            val backIntent = Intent(this, MainActivity::class.java)
-            startActivity(backIntent)
+            finish()
         }
     }
 }
